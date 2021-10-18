2ec700636b8a03fb61ae67de8e6a66fb51a8c37e
Dan Isaev <dkikrlv2@mail.ru>
2021-10-18 04:31:47 +0300

    Main page interface

--- front/src/components/main/main.component.tsx
+++ front/src/components/main/main.component.tsx
@@ -1,41 +1,152 @@
 import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { useHistory } from 'react-router';
+import { useDispatch } from 'react-redux';
 import { ThunkDispatch } from 'redux-thunk';
 import { AnyAction } from '@reduxjs/toolkit';
-import { Link } from '@mui/material';
+
+import {
+    Button,
+    Select,
+    MenuItem,
+    FormControl,
+    InputLabel,
+    Divider,
+    Card,
+    CardContent,
+    Typography,
+    Grid
+} from '@mui/material';
+
 import { RootState } from '~/store/rootReducer';
-import { deleteCurrentUserAction, getCurrentUserAction } from '~/store/user/actions';
+import { getCurrentUserAction } from '~/store/user/actions';
 
 import './main.component.scss';
 
 export const MainComponent = (): JSX.Element => {
-    const user = useSelector((state: RootState) => state.user);
     const dispatch = useDispatch<ThunkDispatch<RootState, void, AnyAction>>();
-    const history = useHistory();
-
-    const handleLogout = async () => {
-        await dispatch(deleteCurrentUserAction());
-    };
 
     useEffect(() => {
         dispatch(getCurrentUserAction());
     }, []);
 
     return (
-        <div className="page-container">
-            <p>Главная страница</p>
-            {user.id > 0
-            ?
-                <div>
-                    <div>Вы вошли как {user.username}</div>
-                    <Link onClick={handleLogout}>Выйти</Link>
-                </div>
-            :   <div>
-                    <div>Вы неавторизованы</div>
-                    <Link href="/login">Войти</Link>
-                </div>
-            }
-        </div>
+        <Grid container justifyContent="center" mt={2}>
+            <Grid item xs={6} container direction="column" rowSpacing={2} columnSpacing={2} p={2}>
+                <Grid container item alignItems="center" rowSpacing={2} columnSpacing={2} mb={5}>
+                    <Grid item>
+                        <FormControl>
+                            <InputLabel id="searchType">Тип поиска</InputLabel>
+                            <Select labelId="searchType" label="Тип поиска" value={'guaranteed'}>
+                                <MenuItem value={'guaranteed'}>Гарантированный</MenuItem>
+                                <MenuItem value={'fast'}>Быстрый</MenuItem>
+                                <MenuItem value={'perspective'}>Перспективный</MenuItem>
+                            </Select>
+                        </FormControl>
+                    </Grid>
+                    <Grid item>
+                        <Button variant="contained" color="success">Подобрать сервис</Button>
+                    </Grid>
+                    <Grid item>
+                        <Button variant="contained">Моя траектория</Button>
+                    </Grid>
+                </Grid>
+                <Grid item container direction="column" rowSpacing={2} columnSpacing={2}>
+                    <Grid item>
+                        <Card>
+                            <CardContent>
+                                <Grid container spacing={2} alignItems="center">
+                                    <Grid item xs={5}>
+                                        <Typography variant="h6">
+                                            Рекомендуемый сервис
+                                        </Typography>
+                                        <Typography variant="body1">
+                                            Описание
+                                        </Typography>
+                                    </Grid>
+                                    <Grid item xs={5}>
+                                        <Typography variant="body1" color="success.main">
+                                            Оценка: хорошо<br />
+                                            По скорости: средне<br />
+                                            Рискованность: высокая
+                                        </Typography>
+                                    </Grid>
+                                </Grid>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                    <Grid item>
+                        <Divider />
+                    </Grid>
+                    <Grid item>
+                        <Card>
+                            <CardContent>
+                                <Grid container spacing={2} alignItems="center">
+                                    <Grid item xs={5}>
+                                        <Typography variant="h6">
+                                            Рекомендуемый сервис
+                                        </Typography>
+                                        <Typography variant="body1">
+                                            Описание
+                                        </Typography>
+                                    </Grid>
+                                    <Grid item xs={5}>
+                                        <Typography variant="body1" color="primary">
+                                            Оценка: хорошо<br />
+                                            По скорости: средне<br />
+                                            Рискованность: высокая
+                                        </Typography>
+                                    </Grid>
+                                </Grid>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                    <Grid item>
+                        <Card>
+                            <CardContent>
+                                <Grid container spacing={2} alignItems="center">
+                                    <Grid item xs={5}>
+                                        <Typography variant="h6">
+                                            Рекомендуемый сервис
+                                        </Typography>
+                                        <Typography variant="body1">
+                                            Описание
+                                        </Typography>
+                                    </Grid>
+                                    <Grid item xs={5}>
+                                        <Typography variant="body1" color="warning.main">
+                                            Оценка: хорошо<br />
+                                            По скорости: средне<br />
+                                            Рискованность: высокая
+                                        </Typography>
+                                    </Grid>
+                                </Grid>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                    <Grid item>
+                        <Card>
+                            <CardContent>
+                                <Grid container spacing={2} alignItems="center">
+                                    <Grid item xs={5}>
+                                        <Typography variant="h6">
+                                            Рекомендуемый сервис
+                                        </Typography>
+                                        <Typography variant="body1">
+                                            Описание
+                                        </Typography>
+                                    </Grid>
+                                    <Grid item xs={5}>
+                                        <Typography variant="body1" color="error">
+                                            Оценка: хорошо<br />
+                                            По скорости: средне<br />
+                                            Рискованность: высокая
+                                        </Typography>
+                                    </Grid>
+                                </Grid>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                </Grid>
+            </Grid>
+        </Grid>
     );
 };
