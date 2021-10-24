25f961695d27d21b636f170ee28b4a3f2b0e184f
Dan Isaev <dkikrlv2@mail.ru>
2021-10-24 15:33:05 +0300

    service-list update, package.json fix

--- front/src/components/service-list/service-list.component.tsx
+++ front/src/components/service-list/service-list.component.tsx
@@ -1,5 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import { Alert, Button, Card, CardContent, Grid, Typography } from '@mui/material';
+import { styled } from '@mui/material/styles';
+
+import {
+    Alert, Button, Card, CardContent, Grid, Typography,
+    FormControl, InputLabel, MenuItem, Select, Box
+} from '@mui/material';
+
 import { findServices } from "~/api";
 import { CommonError } from "~/types";
 
@@ -56,26 +62,48 @@ export const ServiceListComponent = (): JSX.Element => {
         }
     };
 
+    const BottomBox = styled(Box)(({ theme }) => ({
+        padding: 2,
+        [theme.breakpoints.up('lg')]: {
+            width: '50%',
+        },
+    }));
+
     const renderService = (item: string, index: number): JSX.Element => {
         return (
-            <Grid item key={`${item} ${index}`}>
-                <Card>
-                    <CardContent>
-                        <Grid container alignItems="center" columnSpacing={2} className="card-content">
-                            <Grid item className="card-content__item">
-                                <Typography variant="h6">
-                                    {item}
-                                </Typography>
-                                <Typography variant="body1">
-                                    Описание
-                                </Typography>
+            <Grid item container alignItems="center" spacing={2} key={`${item} ${index}`}>
+                <Grid item xs={12} lg={8}>
+                    <Card>
+                        <CardContent>
+                            <Grid container alignItems="center" columnSpacing={2} className="card-content">
+                                <Grid item className="card-content__item">
+                                    <Typography variant="h6">
+                                        {item}
+                                    </Typography>
+                                    <Typography variant="body1">
+                                        Описание
+                                    </Typography>
+                                </Grid>
+                                <Grid item className="card-content__item">
+                                    {renderMark(index)}
+                                </Grid>
                             </Grid>
-                            <Grid item className="card-content__item">
-                                {renderMark(index)}
-                            </Grid>
-                        </Grid>
-                    </CardContent>
-                </Card>
+                        </CardContent>
+                    </Card>
+                </Grid>
+                <Grid item container xs={12} lg={2}>
+                    <Button fullWidth variant="contained" color="primary">Подобрать партнера</Button>
+                </Grid>
+                <Grid item xs={12} lg={2}>
+                    <FormControl fullWidth>
+                        <InputLabel id="searchType">Тип поиска</InputLabel>
+                        <Select labelId="searchType" label="Тип поиска" value={'active'}>
+                            <MenuItem value={'active'}>Действующие программы</MenuItem>
+                            <MenuItem value={'opt2'}>Опция 2</MenuItem>
+                            <MenuItem value={'opt3'}>Опция 3</MenuItem>
+                        </Select>
+                    </FormControl>
+                </Grid>
             </Grid>
         );
     }
@@ -91,9 +119,40 @@ export const ServiceListComponent = (): JSX.Element => {
                 error ? (
                     <Alert severity="error">{error}</Alert>
                 ) : services.length > 0 ? (
-                    <Grid item container direction="column" rowSpacing={2} columnSpacing={2}>
-                        {services.map(renderService)}
-                    </Grid>
+                    <>
+                        <Grid item container direction="column" rowSpacing={{ xs: 6, lg: 3 }} columnSpacing={2}>
+                            <Grid item>
+                                <Typography variant="h3">Рекомендованные сервисы</Typography>
+                            </Grid>
+                            {services.map(renderService)}
+                            <Grid item container direction="column" rowSpacing={{ xs: 6, lg: 3 }} columnSpacing={2}>
+                                <Grid item>
+                                    <BottomBox>
+                                        <Typography color="primary" fontSize={24} fontWeight="bold" mb={3}>
+                                            Чтобы наиболее точно подобрать сервисы, подходящие вашей ситуации, пожалуйста, пройдите опрос
+                                        </Typography>
+                                        <Grid item container spacing={2}>
+                                            <Grid item>
+                                                <Button variant="contained" color="primary">Пройти опрос</Button>
+                                            </Grid>
+                                            <Grid item>
+                                                <Button variant="contained" color="primary">Продолжить самостоятельно</Button>
+                                            </Grid>
+                                        </Grid>
+                                    </BottomBox>
+                                    
+                                </Grid>
+                                {/* <Grid item container spacing={2}>
+                                    <Grid item>
+                                        <Button variant="contained" color="primary">Пройти опрос</Button>
+                                    </Grid>
+                                    <Grid item>
+                                        <Button variant="contained" color="primary">Продолжить самостоятельно</Button>
+                                    </Grid>
+                                </Grid> */}
+                            </Grid>
+                        </Grid>
+                    </>
 
                 ) : (
                     <Alert severity="info">
