2df15ce4a0c38fee95c20c4401589b479f66b70a
Kirill Kazakov <gorgchap@gmail.com>
2021-10-24 17:57:13 +0300

    survey modal (new version)

--- front/src/components/survey-modal/survey-modal.component.tsx
+++ front/src/components/survey-modal/survey-modal.component.tsx
@@ -1,39 +1,82 @@
-import React from 'react';
-import { useHistory } from 'react-router-dom';
-import {Button, Modal, Typography} from '@mui/material';
-
+import React, {useEffect, useMemo, useState} from 'react';
+import { Formik } from 'formik';
+import { Button, FormControl, FormControlLabel, FormLabel, Modal, Radio, RadioGroup, Typography } from '@mui/material';
+import { getQuestions } from '~/api';
+import { SurveyValues } from "~/types";
 import './survey-modal.component.scss';
 
 interface SurveyModalComponentProps {
+    onSubmit: (values: SurveyValues) => void;
     open: boolean;
     setOpen: (value: boolean) => void;
 }
 
-export const SurveyModalComponent = ({ open, setOpen }: SurveyModalComponentProps): JSX.Element => {
-    const history = useHistory();
+interface FormValues {
+    [key: string]: string;
+}
 
-    const openCompanyFrame = () => {
-        setOpen(false);
-        history.push('/company-frame');
+export const SurveyModalComponent = ({ onSubmit, open, setOpen }: SurveyModalComponentProps): JSX.Element => {
+    const [questions, setQuestions] = useState<string[]>([]);
+
+    const initialValues = useMemo((): FormValues => {
+        return questions.reduce((acc, item) => Object.assign(acc, { [item]: '0' }), {});
+    }, [questions]);
+
+    const handleGetQuestions = async () => {
+        try {
+            const questions = await getQuestions();
+            setQuestions(questions);
+        } catch (err) {
+            console.error(err);
+        }
     };
+
+    const onFormSubmit = async (values: FormValues) => {
+        setOpen(false);
+        onSubmit(values);
+    };
+
+    useEffect(() => {
+        handleGetQuestions();
+    }, []);
 
     return (
         <Modal open={open} onClose={() => setOpen(false)}>
             <div className="survey-modal">
-                <Typography variant="h6" component="h2">
+                <Typography variant="h6" component="h2" mb={1}>
                     Подбор сервисов
                 </Typography>
-                <Typography sx={{ mt: 2 }}>
-                    Чтобы наиболее точно выбрать подходящие Вашей ситуации сервисы, пожалуйста, пройдите опрос.
-                </Typography>
-                <div className="survey-modal__options">
-                    <Button color="primary" type="button" variant="contained">
-                        Пройти опрос
-                    </Button>
-                    <Button color="info" type="button" variant="contained" onClick={openCompanyFrame}>
-                        Продолжить самостоятельно
-                    </Button>
-                </div>
+                <Formik initialValues={initialValues} onSubmit={onFormSubmit}>
+                    {props => (
+                        <form onSubmit={props.handleSubmit} noValidate>
+                            <div className="survey-modal__questions">
+                                {
+                                    questions.map(question => (
+                                        <FormControl className="question-item">
+                                            <FormLabel className="question-item__left">
+                                                {question}
+                                            </FormLabel>
+                                            <RadioGroup
+                                                className="question-item__right"
+                                                name={question}
+                                                onChange={props.handleChange}
+                                                value={props.values[question]}
+                                            >
+                                                <FormControlLabel value="1" label="Да" control={<Radio />} />
+                                                <FormControlLabel value="0" label="Нет" control={<Radio />} />
+                                            </RadioGroup>
+                                        </FormControl>
+                                    ))
+                                }
+                            </div>
+                            <div className="survey-modal__options">
+                                <Button color="primary" type="submit" variant="contained">
+                                    Сохранить
+                                </Button>
+                            </div>
+                        </form>
+                    )}
+                </Formik>
             </div>
         </Modal>
     )
