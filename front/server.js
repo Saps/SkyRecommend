7879c29f62378ae5d8b45909f23a4c177bb47e61
Сапегин Сергей Владимирович <sapegin@2035.university>
2021-10-24 21:20:01 +0300

    port

--- front/server.js
+++ front/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const favicon = require('express-favicon');
 const path = require('path');
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8180;
 const app = express();
 
 app.use(favicon(__dirname + '/build/favicon.ico'));
