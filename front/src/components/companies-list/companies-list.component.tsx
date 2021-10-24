d66596d73954e5eb2f6d5384a4c1856c90443c93
Dan Isaev <dkikrlv2@mail.ru>
2021-10-24 22:29:28 +0300

    companies-list: added info modal

--- front/src/components/companies-list/companies-list.component.tsx
+++ front/src/components/companies-list/companies-list.component.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import {
-    Alert, Button, Card, CardContent, Grid, Typography, Rating,
+    Alert, Button, Card, CardContent, Grid, Typography, Rating, Modal,
     FormControl, InputLabel, MenuItem, Select, Box, SelectChangeEvent
 } from '@mui/material';
 
@@ -9,10 +9,21 @@ import { CommonError, CompanyCandidate } from '~/types';
 
 import './companies-list.component.scss';
 
+const modalStyle = {
+    position: 'absolute' as 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 600,
+    bgcolor: 'background.paper',
+    p: 4,
+  };
+
 export const CompaniesListComponent = (): JSX.Element => {
     const [error, setError] = useState<string>();
     const [services, setServices] = useState<CompanyCandidate[]>([]);
     const [searchType, setSearchType] = useState<string>('active');
+    const [moreItem, setMoreItem] = useState<CompanyCandidate | null>(null);
 
     const onSearch = async () => {
         try {
@@ -29,6 +40,14 @@ export const CompaniesListComponent = (): JSX.Element => {
         const value: string = (e.target as HTMLSelectElement).value;
 
         setSearchType(value);
+    };
+
+    const handleDisplayMore = (item: CompanyCandidate) => {
+        setMoreItem(item);
+    };
+
+    const handleModalClose = () => {
+        setMoreItem(null);
     };
 
     const renderMark = (rating: number): JSX.Element => {
@@ -57,6 +76,9 @@ export const CompaniesListComponent = (): JSX.Element => {
                             <Grid item className="card-content__item">
                                 {renderMark(item.rating)}
                             </Grid>
+                            <Grid item className="card-content__item">
+                                <Button onClick={() => handleDisplayMore(item)}>подробнее</Button>
+                            </Grid>
                         </Grid>
                     </CardContent>
                 </Card>
@@ -65,44 +87,71 @@ export const CompaniesListComponent = (): JSX.Element => {
     }
 
     return (
-        <Grid container item direction="column" p={2} xs={12} sm={10} md={8}>
-            {error
-            ? <Alert severity="error">{error}</Alert>
-            : <Grid item container direction="column" spacing={2}>
-                <Grid item>
-                    <Box component="h3">Рекомендованные компании</Box>
-                </Grid>
-                <Grid item container alignItems="center" justifyContent="center" spacing={2}>
+        <>
+            <Grid container item direction="column" p={2} xs={12} sm={10} md={8}>
+                {error
+                ? <Alert severity="error">{error}</Alert>
+                : <Grid item container direction="column" spacing={2}>
                     <Grid item>
-                        <Button variant="contained" color="primary" onClick={onSearch}>
-                            Подобрать компании
-                        </Button>
+                        <Box component="h3">Рекомендованные компании</Box>
                     </Grid>
-                    <Grid item>
-                        <FormControl>
-                            <InputLabel id="searchType">Тип поиска</InputLabel>
-                            <Select
-                                labelId="searchType"
-                                label="Тип поиска"
-                                value={searchType}
-                                onChange={handleSearchTypeChange}
-                            >
-                                <MenuItem value={'active'}>Действующие компании</MenuItem>
-                                <MenuItem value={'all'}>Все компании</MenuItem>
-                            </Select>
-                        </FormControl>
+                    <Grid item container alignItems="center" justifyContent="center" spacing={2}>
+                        <Grid item>
+                            <Button variant="contained" color="primary" onClick={onSearch}>
+                                Подобрать компании
+                            </Button>
+                        </Grid>
+                        <Grid item>
+                            <FormControl>
+                                <InputLabel id="searchType">Тип поиска</InputLabel>
+                                <Select
+                                    labelId="searchType"
+                                    label="Тип поиска"
+                                    value={searchType}
+                                    onChange={handleSearchTypeChange}
+                                >
+                                    <MenuItem value={'active'}>Действующие компании</MenuItem>
+                                    <MenuItem value={'all'}>Все компании</MenuItem>
+                                </Select>
+                            </FormControl>
+                        </Grid>
                     </Grid>
-                </Grid>
-                {services.length > 0
-                ? services.map(renderService)
-                : <Grid item>
-                    <Alert severity="info">
-                        После нажатия на кнопку "Подобрать компании" здесь отобразятся результаты поиска.
-                    </Alert>
+                    {services.length > 0
+                    ? services.map(renderService)
+                    : <Grid item>
+                        <Alert severity="info">
+                            После нажатия на кнопку "Подобрать компании" здесь отобразятся результаты поиска.
+                        </Alert>
+                    </Grid>
+                    }
                 </Grid>
                 }
             </Grid>
-            }
-        </Grid>
+            <Modal open={Boolean(moreItem)} onClose={handleModalClose}>
+                <Box sx={modalStyle} component="div">
+                    <Grid container direction="column" rowSpacing={3}>
+                        <Grid item>
+                            <Typography variant="h6">{moreItem?.name}</Typography>
+                            <Typography variant="body1">{moreItem?.type}</Typography>
+                        </Grid>
+                        {moreItem?.algos.length
+                        ? <>
+                            <Grid item>
+                                <Box component="h4" sx={{ textAlign: 'center' }}>Использованные алгоритмы</Box>
+                            </Grid>
+                            {moreItem?.algos.map(entry => 
+                                <Grid item>
+                                    <Box component="h4">{entry.a_name}</Box>
+                                    <Box component="div">{entry.a_message}</Box>
+                                </Grid>
+                            )}
+                            
+                        </>
+                        : null}
+                    </Grid>
+                        
+                </Box>
+            </Modal>
+        </>
     );
 };
